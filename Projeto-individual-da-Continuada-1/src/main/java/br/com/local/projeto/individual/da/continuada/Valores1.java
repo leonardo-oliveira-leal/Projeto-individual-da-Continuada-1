06bdceec82853b71d034a8cca58b5ed30a23c199
Leonardo Oliveira Leal <61800482+leonardo-oliveira-leal@users.noreply.github.com>
2020-09-05 12:59:06 -0300

    Projeto Individual
    Projeto Individual da Continuada 1 - LP

---
+++ Projeto-individual-da-Continuada-1/src/main/java/br/com/local/projeto/individual/da/continuada/Valores1.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.local.projeto.individual.da.continuada;
+
+/**
+ *
+ * @author LEONARDOOLIVEIRALEAL
+ */
+public class Valores1 {
+    Double utilizacao = 0.00;
+}
